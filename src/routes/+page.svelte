7c7b55d618cde8869915a4b10a71f23e665603b1
joaosmendess <joaosilva0721@gmail.com>
2024-04-14 21:55:20 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+	
+	<h1>
+		Hello world
+	</h1>
+
+	
+
+</div>
+
+<style>
+	div {
+		display: flex;
+	
+		justify-content: center;
+		height: 100%;
+		width: 100%;
+		align-items: center;
+	
+		background: #000;
+	}
+
+	h1 {
+		
+		color: #ffff;
+	}
+
+	
+
+
+</style>
